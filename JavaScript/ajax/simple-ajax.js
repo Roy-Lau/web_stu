5354b78c4cfd433dde4894501acc1c232c3f2560
roy-lau <roy-lau@foxmail.com>
2017-05-10 18:51:15 +0800

    ES6 Example

---
+++ JavaScript/ajax/simple-ajax.js
@@ -0,0 +1,13 @@
+
+function ajax(path,mothod,success,error) {
+	var xml = new XMLHttpRequest();
+	xml.open(mothod,path,true);
+	xml.send();
+	xml.onload=function(){
+		if (xml.readyState == 4 && xml.status == 200) {
+			success(xml.responseText);
+		}else{
+			error(xml.status);
+		}
+	}
+}
---
+++ php/simple-syntax.php
@@ -0,0 +1,17 @@
+<?php
+	$name = "roy";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<h1><?php echo $name ?></h1>
+</body>
+</html>
+
+<!-- 此文件需要在对应的环境下运行 -->
