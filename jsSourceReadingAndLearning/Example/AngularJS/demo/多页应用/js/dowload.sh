10f65c32103eccb07a9fea05dacc6780d79ae250
roy-lau <roy-lau@foxmail.com>
2018-04-18 19:36:34 +0800

    AngularJs: 整理

---
+++ jsSourceReadingAndLearning/Example/AngularJS/demo/多页应用/js/dowload.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# angular Version1.3.6
+#
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-animate.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-animate.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-animate.min.js.map
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-aria.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-aria.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-aria.min.js.map
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-cookies.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-cookies.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-cookies.min.js.map
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-csp.css
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-loader.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-loader.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-loader.min.js.map
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-messages.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-messages.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-messages.min.js.map
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-mocks.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-resource.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-resource.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-resource.min.js.map
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-route.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-route.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-route.min.js.map
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-sanitize.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-sanitize.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-sanitize.min.js.map
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-scenario.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-touch.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-touch.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular-touch.min.js.map
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular.min.js
+curl -O http://apps.bdimg.com/libs/angular.js/1.3.6/angular.min.js.map
+
+pwd
